6484df1af450e8edbe50aee167051245e3a0b1ce
Gerhard Sletten <gerhardsletten@gmail.com>
2016-05-14 09:50:21 +0200

    ikoner

--- src/containers/MapView/MapView.js
+++ src/containers/MapView/MapView.js
@@ -6,7 +6,8 @@ import {isLoaded, loadOne} from 'redux/modules/maps'
 import {GoogleMapLoader, GoogleMap, Polyline, Marker} from 'react-google-maps'
 import style from './style.css'
 import mapStyle from './mapstyle.json'
-import icon from './icons/icon.svg'
+import startIcon from './icons/start.svg'
+import endIcon from './icons/end.svg'
 import turf from 'turf'
 
 @asyncConnect([{
@@ -63,23 +64,18 @@ export default class MapView extends Component {
     )
   }
 
-  renderInfo (coordinates, point) {
+  renderInfo (coordinates) {
     const {title} = this.props.map
     const course = this.toGeoJSON(coordinates, 'LineString')
-    const athlete = this.toGeoJSON(point)
-    const snapped = turf.pointOnLine(course, athlete)
-    const sliced = turf.lineSlice(this.toGeoJSON(coordinates[0]), snapped, course)
-    const distanceCurrent = turf.lineDistance(sliced, 'kilometers')
     const distanceTotal = turf.lineDistance(course, 'kilometers')
     return (
-      <div className={style.info}>{title}: {distanceTotal.toFixed(2)} km / {distanceCurrent.toFixed(2)} km</div>
+      <div className={style.info}>{title}: {distanceTotal.toFixed(2)} km</div>
     )
   }
 
   renderMap (coordinates) {
     const start = coordinates[0]
     const end = coordinates[coordinates.length - 1]
-    const marker = coordinates[Math.floor(coordinates.length / 2)]
     return (
       <div className={style.container}>
         <GoogleMapLoader
@@ -97,13 +93,12 @@ export default class MapView extends Component {
               <Polyline
                 path={coordinates.map((p) => this.pointToLngLat(p))}
               />
-              <Marker position={this.pointToLngLat(start)} options={{icon: this.pinSymbol(this.color.start)}} />
-              <Marker position={this.pointToLngLat(end)} options={{icon: this.pinSymbol(this.color.end)}} />
-              <Marker position={this.pointToLngLat(marker)} options={{icon: this.svgSymbol()}} />
+              <Marker position={this.pointToLngLat(start)} options={{icon: this.startSymbol(this.color.start)}} />
+              <Marker position={this.pointToLngLat(end)} options={{icon: this.endSymbol(this.color.end)}} />
             </GoogleMap>
           }
         />
-        {this.state.mapInited && this.renderInfo(coordinates, marker)}
+        {this.state.mapInited && this.renderInfo(coordinates)}
       </div>
     )
   }
@@ -132,11 +127,19 @@ export default class MapView extends Component {
     }
   }
 
-  svgSymbol () {
+  startSymbol () {
     return {
-      url: icon,
-      anchor: {x: 12, y: 12},
-      scaledSize: {height: 24, width: 24}
+      url: startIcon,
+      anchor: {x: 16, y: 16},
+      scaledSize: {height: 32, width: 32}
+    }
+  }
+
+  endSymbol () {
+    return {
+      url: endIcon,
+      anchor: {x: 16, y: 16},
+      scaledSize: {height: 32, width: 32}
     }
   }
 
